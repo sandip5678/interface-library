68554033eeccd04923ad2c2350a25c9b60afcb84
mueller <mueller@e652e40c-185c-4641-87fb-3ecfe257c510>
2013-06-04 20:39:00 +0000

    revert
    git-svn-id: https://svn.localdev.cc/svn/shopgate_plugin_library/branches/2.1.x@1009 e652e40c-185c-4641-87fb-3ecfe257c510

--- build_config.php
+++ build_config.php
@@ -2,7 +2,7 @@
 $buildConfig = array (
 	'major' => 2,
 	'minor' => 1,
-	'build' => 27,
+	'build' => 26,
 	'shopgate_library_path' => "",
 	'plugin_name' => "library",
 	'display_name' => "Shopgate Library 2.1.x",
