5f3fb1536eb2102d9aecf63759127b0802ac5332
Timo Ebel <timo.ebel@shopgate.com>
2015-10-28 12:49:59 +0100

    Changed copyright 2012-2014 to 2012-2015.

--- classes/models/AbstractExport.php
+++ classes/models/AbstractExport.php
@@ -2,11 +2,11 @@
 /*
  * Shopgate GmbH
  * http://www.shopgate.com
- * Copyright © 2012-2014 Shopgate GmbH
+ * Copyright © 2012-2015 Shopgate GmbH
  *
  * Released under the GNU General Public License (Version 2)
  * [http://www.gnu.org/licenses/gpl-2.0.html]
-*/
+ */
 
 abstract class Shopgate_Model_AbstractExport extends Shopgate_Model_Abstract {
 
