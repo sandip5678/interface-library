f95e66650520d5fb79c98a8ac7a2c2c0391eef6e
Oliver Müller <oliver.mueller@shopgate.com>
2016-04-19 08:53:33 +0200

    fixed merge conflicts

--- classes/helper/index.php
+++ classes/helper/index.php
@@ -6,4 +6,4 @@
  *
  * Released under the GNU General Public License (Version 2)
  * [http://www.gnu.org/licenses/gpl-2.0.html]
-*/
+ */
--- classes/models/XmlEmptyObject.php
+++ classes/models/XmlEmptyObject.php
@@ -6,7 +6,7 @@
  *
  * Released under the GNU General Public License (Version 2)
  * [http://www.gnu.org/licenses/gpl-2.0.html]
-*/
+ */
 
 /**
  * @class Shopgate_Model_Catalog_XmlEmptyObject
